612d43d974e16494b592117f5af0647fcc09ef46
marramarco.matthieu@gmail.com <marramarco.matthieu@gmail.com>
2021-02-03 17:59:47 +0100

    separated C# from the other repository

--- godot-csharp/Properties/AssemblyInfo.cs
+++ godot-csharp/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Godot Design Pattern demos")]
+[assembly: AssemblyTitle("Godot Design Pattern csharp demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
