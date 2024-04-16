bc28fff761055fbf821008bb3227168553622ed6
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-04-16 13:33:29 +0200

    rename ECS folder to EntityComponent in csharp copy of project

---
+++ godot-csharp/EntityComponent/Game.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System.Collections.Generic;
+
+public class Game : Node
+{
+	private PowerSystem powerSystem = new PowerSystem();
+
+	public override void _Ready()
+	{
+		var sources = GetTree().GetNodesInGroup("power_sources");
+		var sourceList = ToList<Node2D>(sources);
+		var receivers = GetTree().GetNodesInGroup("power_receivers");
+		var receiverList = ToList<Node2D>(receivers);
+		var tileMap = GetNode<TileMap>("TileMap");
+		powerSystem.Setup(sourceList, receiverList, tileMap);
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		powerSystem.Update(delta);
+	}
+
+	private List<T> ToList<T>(Godot.Collections.Array array) where T : Node
+	{
+		var results = new List<T>();
+
+		foreach (var element in array)
+		{
+			results.Add((T)element);
+		}
+
+		return results;
+	}
+}
